2f8238a21cce019e147a186ff36c903aa93b0fd6
Ankit Bajpai <ankit65bajpai@gmail.com>
2023-04-01 14:46:45 +0530

    Developed website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import './index.css'
+import { Route, Routes } from 'react-router-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Item_details from "./Components/Item_details";
+import Book_ticket from "./Components/Book_ticket";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      {/* <Home /> */}
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/item_details" element={<Item_details />} />
+        <Route path="/book_ticket" element={<Book_ticket />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="container-fluid footerDiv">
+            <p>Footer here</p>
+            <p>Copyright ©2023 All rights reserved | This template is made with ♡ by Ankit Bajpai</p>
+        </div>
+    )
+}
+
+export default Footer
